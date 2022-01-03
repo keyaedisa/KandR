7dc64e6689b82f6b79b1bf6ddc804898eb7589a1
key <keyaedisa@gmail.com>
2022-01-02 19:43:03 -0500

    test of vscode git stuff

---
+++ kandrex1-14.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main(){
+
+/* im a fucking failure and skipped this one
+because it was difficult and so was the last and 
+it caused me to get really burnt out. so for the sake
+of progress (and my fucking sanity) to the next we go. */
+
+
+}
