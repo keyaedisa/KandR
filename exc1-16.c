1053e88264ae3cf463ed575ccda7203d6f7eed24
key <keyaedisa@gmail.com>
2022-01-05 06:01:31 -0500

    Upload of current 1.16 progress

--- exc1-16.c
+++ exc1-16.c
@@ -1,40 +1,58 @@
 #include <stdio.h>
-#define MAXLINE 1000
+#define MAXLINE 1000 /* maximum input line length */
 
-/* maximum input line length */
-int getline(char line[], int maxline);
+int fetchLine(char line[], int maxline);
 void copy(char to[], char from[]);
 
+char *tempLineHolder[MAXLINE];
+
 /* print the longest input*/
-main()
-{
-int len;    /* current line length */
-int max;    /*maximum length seen so far */
 
-char line[MAXLINE]; /* current input line */
-char longest[MAXLINE];  /* longest line saved here */
+int main(){
+    int len;                        /* current line length */
+    int max;                        /*maximum length seen so far */
+    int lineLengthStorage[MAXLINE]; /* stores lines lengths */
 
-max = 0;
-while ((len = getline(line, MAXLINE)) > 0)
-    if (len > max) {
-        max = len;
-    copy(longest, line);
+    char line[MAXLINE];             /* current input line */
+    char longest[MAXLINE];          /* longest line saved here */
+    
+    int i = 0; 
+    max = 0;
+
+    while ((len = fetchLine(line, MAXLINE)) > 0){
+        lineLengthStorage[i] = len;
+        ++i;
+        if (len > max) {
+            
+            //lineLengthStorage[i] = len;
+            //++i;
+            max = len;
+            copy(longest, line);
+        }
     }
-
-if (max > 0) /* there was a line */
-    printf("%s", longest);
-return 0;
+    if (max > 0){                      /* there was a line */
+        printf("\n");
+        printf("Longest line: %s", longest);
+        for(int j = 0; j < MAXLINE && lineLengthStorage[j] != 0; ++j){
+            printf("Line number:%d had a length of:%d characters. It reads as follows: %s\n", j+1, lineLengthStorage[j]-1, tempLineHolder[j]);
+        }
+    }
+    return 0;
 }
 
 
 /* getline: read a line into s, return length */
-int getline(char s[],int lim){
-    int c, i;
+int fetchLine(char s[],int lim){
+    int i;
+    char c;
 
-    for (i=0; i < lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
+    for (i=0; i < lim-1 && (c=getchar())!=EOF && c!='\n'; ++i){
         s[i] = c;
+        tempLineHolder[i] = c;
+    }
     if (c == '\n') {
         s[i] = c;
+        tempLineHolder[i] = c;
         ++i;
     }
 s[i] = '\0';
