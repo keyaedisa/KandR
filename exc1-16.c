655009c1cc3783d67e00901d4e3e1aec7684431b
key <keyaedisa@gmail.com>
2022-01-12 18:58:59 -0500

    update

--- exc1-16.c
+++ exc1-16.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #define MAXLINE 1000 /* maximum input line length */
 
+/* excercise 1-16. revise the main routine of the
+longest-line program so it will correctly print
+the length of arbitrary long input lines, and as 
+much as possible of the text. */
+
 int fetchLine(char line[], int maxline);
 void copy(char to[], char from[]);
 
-char* tempLineHolder[MAXLINE];
 
 /*print the longest input*/
 
@@ -12,9 +16,9 @@ int main(){
     int len;                        /* current line length */
     int max;                        /*maximum length seen so far */
     int lineLengthStorage[MAXLINE]; /* stores lines lengths */
-
     char line[MAXLINE];             /* current input line */
     char longest[MAXLINE];          /* longest line saved here */
+    char lineStorage[100][100];     /* stores whole lines for later*/
     
     int i = 0; 
     max = 0;
@@ -22,20 +26,21 @@ int main(){
     while ((len = fetchLine(line, MAXLINE)) > 0){
         lineLengthStorage[i] = len;
         ++i;
+
         if (len > max) {
-            
-            //lineLengthStorage[i] = len;
-            //++i;
             max = len;
             copy(longest, line);
         }
     }
+
     if (max > 0){                      /* there was a line */
         printf("\n");
         printf("Longest line: %s", longest);
+        
         for(int j = 0; j < MAXLINE && lineLengthStorage[j] != 0; ++j){
-            printf("Line number:%d had a length of:%d characters. It reads as follows: %d\n", j+1, lineLengthStorage[j]-1, *tempLineHolder[j]);
+            printf("Line number:%d had a length of:%d characters.\n", j+1, lineLengthStorage[j]-1);
         }
+
     }
     return 0;
 }
@@ -44,15 +49,14 @@ int main(){
 /* getline: read a line into s, return length */
 int fetchLine(char s[],int lim){
     int i;
-    char c;
+    int c;
 
     for (i=0; i < lim-1 && (c=getchar())!=EOF && c!='\n'; ++i){
         s[i] = c;
-        *tempLineHolder[i] = c;
+        
     }
     if (c == '\n') {
         s[i] = c;
-        //*tempLineHolder[i] = c;
         ++i;
     }
 s[i] = '\0';
