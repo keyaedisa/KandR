ca5d624901320096cd81fbf1a11ca0fe507daabd
key <keyaedisa@gmail.com>
2022-01-12 22:59:55 -0500

    finished 1-16. even made it spit back whole lines

--- exc1-16.c
+++ exc1-16.c
@@ -8,9 +8,16 @@ much as possible of the text. */
 
 int fetchLine(char line[], int maxline);
 void copy(char to[], char from[]);
+void addToStorage(char line[]);
+void printCurrentLine(int lineNumber);
 
-
+char lineStorage[MAXLINE][MAXLINE];     /* stores whole lines for later*/
+int lineNumber = 0;
+//int z = 0;
 /*print the longest input*/
+
+
+
 
 int main(){
     int len;                        /* current line length */
@@ -18,14 +25,19 @@ int main(){
     int lineLengthStorage[MAXLINE]; /* stores lines lengths */
     char line[MAXLINE];             /* current input line */
     char longest[MAXLINE];          /* longest line saved here */
-    char lineStorage[100][100];     /* stores whole lines for later*/
     
+    
+    printf("hello, world\n");
+
     int i = 0; 
     max = 0;
 
     while ((len = fetchLine(line, MAXLINE)) > 0){
+        addToStorage(line);
+        ++lineNumber;
         lineLengthStorage[i] = len;
         ++i;
+        //++z;
 
         if (len > max) {
             max = len;
@@ -33,16 +45,17 @@ int main(){
         }
     }
 
-    if (max > 0){                      /* there was a line */
+    if (max > 0){                     
         printf("\n");
         printf("Longest line: %s", longest);
         
         for(int j = 0; j < MAXLINE && lineLengthStorage[j] != 0; ++j){
-            printf("Line number:%d had a length of:%d characters.\n", j+1, lineLengthStorage[j]-1);
+            printf("Line number %d had a length of %d characters.\n", j+1, lineLengthStorage[j]-1);
+            printCurrentLine(j);
         }
 
     }
-    return 0;
+     return 0;
 }
 
 
@@ -59,8 +72,8 @@ int fetchLine(char s[],int lim){
         s[i] = c;
         ++i;
     }
-s[i] = '\0';
-return i;
+    s[i] = '\0';
+    return i;
 }
 /* copy: copy 'from' into 'to'; assume to is big enough */
 void copy(char to[], char from[]){
@@ -70,4 +83,29 @@ void copy(char to[], char from[]){
     
     while ((to[i] = from[i]) != '\0')
         ++i;
+}
+
+// addToStorage: adds LINE too lineStorage
+
+void addToStorage(char line[]){
+    int z = 0;
+    
+    while(line[z] != '\n'){
+        lineStorage[lineNumber][z] = line[z];
+        ++z;
+    }
+    ++z;
+    lineStorage[lineNumber][z] = '\n';  
+}
+
+// printCurrentLine: prints line it was called on
+
+void printCurrentLine(int lineNumber){
+    printf("Line reads as follows: ");
+    int z = 0;
+    while(lineStorage[lineNumber][z] != '\n'){
+        printf("%c", lineStorage[lineNumber][z]);
+        ++z;
+    }
+    printf("\n");
 }
