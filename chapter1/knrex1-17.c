f0e7199674a4281de22d3213eb6c14a9b0acf7a2
key <keyaedisa@gmail.com>
2022-01-18 00:12:34 -0500

    testing 1-17

--- chapter1/knrex1-17.c
+++ chapter1/knrex1-17.c
@@ -1,9 +1,39 @@
 #include <stdio.h>
+#include <stdbool.h>
+#define MAXLINE 1000
 /* Exercise 1-17.  Write a program to print
 ** all input lines that are longer than 80 characters. 
-*/ 
+*/
+
+bool abort = false; 
+int lineNumber = 0;
+char lineStorage[MAXLINE][MAXLINE];
+char line[MAXLINE];
+
+void lineSaver(char inputLine[]);
 
 
 void main(){
 
+    while(abort == 0){
+        fgets(line, MAXLINE, stdin);
+        lineSaver(line);
+    }
+
+    printf("%s", line);
+
+}
+
+void lineSaver(char inputLine[]){
+    int z = 0;
+
+    if(inputLine[z] != EOF){    
+        while(inputLine[z] != '\n'){
+            lineStorage[lineNumber][z] = inputLine[z];
+            ++z;
+        }
+        ++z;
+        lineStorage[lineNumber][z] = '\n';
+    } 
+    else { abort = true;}  
 }
