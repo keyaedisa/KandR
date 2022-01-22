e8a022cf099f4d1ee6a2d03282dc145b601d705c
key <keyaedisa@gmail.com>
2022-01-21 20:54:59 -0500

    1.18

---
+++ chapter1/1.18.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "myFunctions.h"
+#define MAXLINE 1000   /* maximum input line length */
+/* this took 5 mins for some reason */
+
+int main(){
+    char lineStorage[MAXLINE][MAXLINE];
+
+    lineSaver(lineStorage);
+    for(int x = 0; lineStorage[x][0] != '\0'; ++x){
+        for(int y = 0; lineStorage[x][y]!= '\n' && lineStorage[x][0]!='\n'; ++y){
+            if(lineStorage[x][y]!=' ' && lineStorage[x][y]!='\t'){
+                printf("%c",lineStorage[x][y]);
+            }
+        }
+    }
+
+
+
+
+    return 0;
+}
