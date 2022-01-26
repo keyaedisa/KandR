55242d7412c1b485b55268f301fb294d17af21e9
key <keyaedisa@gmail.com>
2022-01-26 15:20:24 -0500

    desktop push

---
+++ chapter1/1.20.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "myFunctions.h"
+
+int main(){
+    detab();
+    return 0;
+}
--- chapter1/myFunctions.h
+++ chapter1/myFunctions.h
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #define MAXLINE 1000 //max length of 2nd dimension
+#define SPACE ' '
+#define TAB '\t'
+#define TABSIZE 8
 
 
 // Saves lines into array, line by line, with max 1000 length
@@ -32,9 +35,8 @@ void printLines(char (*inputArray)[MAXLINE]){
 void reverses(){
     char c;
     char currentLine[MAXLINE];
-    int x;
     
-    for(x = 0; (c = getchar())!= EOF; ++x){
+    for(int x = 0; (c = getchar())!= EOF; ++x){
         if(c != '\n'){
             currentLine[x] = c;
         }
@@ -48,4 +50,34 @@ void reverses(){
             printf("\n\n");
         }
     }    
+}
+
+void detab(){
+    char c;
+    char currentLine[MAXLINE];
+    
+    for(int x, y, z = 0; (c = getchar())!= EOF; ++x){
+        if(c != '\n'){
+            currentLine[x] = c;
+        }
+        if(c == TAB){
+            y = x;
+            while(y < TABSIZE){
+                currentLine[y] = SPACE;
+            }
+            x = x + y;
+            y = 0;
+        }
+        if(c == '\n'){
+            currentLine[x] = c;
+            ++x;
+            printf("\n");
+            while(currentLine[z]!='\n'){
+                printf("%c", currentLine[y]);
+                ++z;
+            }
+            z = x;
+            printf("\n\n");
+        }
+    }      
 }
