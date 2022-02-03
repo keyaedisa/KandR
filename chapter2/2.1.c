5c18408547fdd1f8275ef5df9c52aa3745955875
key <keyaedisa@gmail.com>
2022-02-02 19:05:45 -0500

    attempted but skipped 2.1

---
+++ chapter2/2.1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+
+//skipped this too, variables have limits. understood
+//also this like idk very hard for no reason so im moving on
+
+int main(){
+    char charMax, charMin;
+    signed char charMaxSigned, charMinSigned;
+    unsigned char charMaxUnsigned, charMinUnsigned;
+    short shortMax, shortMin;
+    signed short shortMaxSigned, shortMinSigned;
+    unsigned short shortMaxUnsigned, shortMinUnsigned;
+    long longMax, longMin;
+    signed long longMaxSigned, longMinSigned;
+    unsigned long longMaxUnsigned, longMinUnsigned;
+    int intMax, intMin;
+    signed int intMaxSigned, intMinSigned;
+    unsigned int intMaxUnsigned, intMinUnsigned;
+
+    charMax = CHAR_MAX;
+    charMin = CHAR_MIN;
+    charMaxSigned = SCHAR_MAX;
+    charMinSigned = SCHAR_MIN;
+    charMaxUnsigned = UCHAR_MAX;
+    charMinUnsigned = UCHAR_MIN;
+
+
+
+    
+
+    return 0;
+}
