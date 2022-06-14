aceac83669e06ad37ed740e742040e090935284a
keyaedisa <keyaedisa@gmail.com>
2022-06-13 22:18:11 -0400

    bbl

--- chapter2/2.3.c
+++ chapter2/2.3.c
@@ -2,4 +2,20 @@
 #include "myFunctions.h"
 
 
-//
+/*
+
+Write a function -htoi(s)-, which converts a string of hexadecimal digits
+(including an optional 0x or 0X) into its equivalent integer value. 
+The allowable digits are 0 through 9, a through f, and A through F.
+
+*/
+
+void main(){
+	char hexadecimal[];
+
+    printf("\nEnter hex to be converted to its integer value\n\n");
+    linesaver(hexadecimal);
+	htoi(hexadecimal);
+
+
+}
--- myFunctions.h
+++ myFunctions.h
@@ -51,3 +51,36 @@ void reverses(){
         }
     }    
 }
+
+
+/* getline:  read a line into s, return length  */
+int getsline(char s[],int lim){
+    int c, i;
+    
+    for (i=0; i < lim-1 && (c=getchar())!=EOF && c!='\n'; ++i){
+        s[i] = c;
+    }    
+    if (c == '\n'){
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
+
+/* copy:  copy 'from' into 'to'; assume to is big enough */
+void copy(char to[], char from[]){
+    int i;
+    i = 0;
+    while ((to[i] = from[i]) != '\0')
+        ++i;
+}
+
+
+// converts hex numbers into their integer value
+int htoi(){
+    
+
+
+
+}
